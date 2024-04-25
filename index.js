e5c339aa0d1b8ca5e34bf09defa981e6bac928db
Devandre007 <andremateus803@gmail.com>
2024-04-25 09:54:11 -0300

    projeto pessoal

---
+++ index.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    console.log('Formulário enviado!');
+    // Aqui você pode adicionar código para enviar os dados do formulário para um servidor
+});
